fa70f54447cf77861b168ffbe158b2e7678f4d9c
cafebabe007 <shilovana@gmail.com>
2017-09-21 19:25:09 -0700

    cleaning up patrial form for reCAPTCHA

--- Halloween/E-greeting_mvc/Controllers/HomeController.cs
+++ Halloween/E-greeting_mvc/Controllers/HomeController.cs
@@ -25,49 +25,12 @@ namespace E_greeting_mvc.Controllers
 
             var response = Request["g-recaptcha-response"];
             //secret that was generated in key value pair
-            const string secret = "YOUR KEY VALUE PAIR";
+            const string secret = "6LfVpjEUAAAAAK0FdygAgh0P1gZ8QU24ildwT86r";
 
             var client = new WebClient();
             var reply =
                 client.DownloadString(
                     string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, response));
-
-            /*var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(reply);
-
-            //when response is false check for the error message
-            if (!captchaResponse.Success)
-            {
-                if (captchaResponse.ErrorCodes.Count <= 0) return View();
-
-                var error = captchaResponse.ErrorCodes[0].ToLower();
-                switch (error)
-                {
-                    case ("missing-input-secret"):
-                        ViewBag.Message = "The secret parameter is missing.";
-                        break;
-                    case ("invalid-input-secret"):
-                        ViewBag.Message = "The secret parameter is invalid or malformed.";
-                        break;
-
-                    case ("missing-input-response"):
-                        ViewBag.Message = "The response parameter is missing.";
-                        break;
-                    case ("invalid-input-response"):
-                        ViewBag.Message = "The response parameter is invalid or malformed.";
-                        break;
-
-                    default:
-                        ViewBag.Message = "Error occured. Please try again";
-                        break;
-                }
-            }
-            else
-            {
-                ViewBag.Message = "Valid";
-            }*/
-
-
-
 
             return View();
         }
