66d559fc716e3c4f9aa178544be315df88ea6e1c
cafebabe007 <shilovanna@gmail.com>
2017-09-30 22:31:04 -0700

    adding character counter for subject and message field

---
+++ Halloween/E-greeting_mvc/js/site.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+
+   
+    var length = 0; 
+
+    $('input,textarea').keyup(function () {
+        var maxLength = $(this).attr("maxlength");
+        var length = $(this).val().length;
+        var length = maxLength - length;
+        $(this).siblings(".countdown").text(length +" characters left");
+    });
+
+    $('input,textarea').change(function () {
+        $(this).siblings(".countdown").text("");
+    })
+
+    $('#message').keyup(function () {
+        var maxLength = $(this).attr("maxlength");
+        var length = $(this).val().length;
+        var length = maxLength - length;
+        $(this).closest("#countdown").text(length);
+    });
+
+   
+  
+    $('#subject').keyup(function () {
+        var maxLength = $(this).attr("maxlength");
+        var length = $(this).val().length;
+        var length = maxLength - length;
+        $('#countdown_subject').text(length);
+    });
+}); 
